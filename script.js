52fd8935a5608d31c92c1b1e6d553e5df912d110
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-16 17:54:35 -0500

    age example

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-// browser window prompt method
-console.log('is that toby keith?')
-let promptText = "what is your name?"
-let defaultPromptValue = 'Toby Keith'
-let answer = prompt(`This is the prompt - ${promptText} Is it ${defaultPromptValue}`, defaultPromptValue)
-console.log("What is thoust nameth?", answer)
+// // browser window prompt method
+// console.log('is that toby keith?')
+// let promptText = "what is your name?"
+// let defaultPromptValue = 'Toby Keith'
+// let answer = prompt(`This is the prompt - ${promptText} Is it ${defaultPromptValue}`, defaultPromptValue)
+// console.log("What is thoust nameth?", answer)
 
-// readline node.js example
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-rl.question('What is your name? ', (answer) => {
-    console.log(`Hello ${answer}`);
-    rl.close();
-});
+// // readline node.js example
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+// rl.question('What is your name? ', (answer) => {
+//     console.log(`Hello ${answer}`);
+//     rl.close();
+// });
+
+// practical example
+// let age = parseInt(prompt("What is your age?"));
+// rl.question('What is your age? ', (answer) => {
+//     let yearsUntilOneHundred = 100 - answer;
+
+//     console.log(`You have ${yearsUntilOneHundred} years until you are 100 years old.`);
+//     rl.close();
+// });
+// console.log(`You have ${yearsUntilOneHundred} years until you are 100 years old.`)
