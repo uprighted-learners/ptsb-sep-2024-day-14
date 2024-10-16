44573ff803e388c8e3c6f657e2c099f6091d3011
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-16 18:23:55 -0500

    callback examples

--- script.js
+++ script.js
@@ -25,3 +25,18 @@
 //     rl.close();
 // });
 // console.log(`You have ${yearsUntilOneHundred} years until you are 100 years old.`)
+
+
+// CALLBACK EXAMPLES
+function greeting(name, age, favColor) {
+    console.log(`Hello ${name}! You are ${age} years old and your favorite color is ${favColor}.`);
+}
+
+function processUserInput(z) {
+    let y = prompt("What is your name?");
+    let a = prompt("What is your age?");
+    let b = prompt("What is your favorite color?");
+    z(y, a, b);
+}
+
+processUserInput(greeting);
