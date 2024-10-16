26553aa92cc2ace18b044d560f6041fc574a55ef
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-16 17:35:45 -0500

    browser and node examples of IO

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+// browser window prompt method
+console.log('is that toby keith?')
+let promptText = "what is your name?"
+let defaultPromptValue = 'Toby Keith'
+let answer = prompt(`This is the prompt - ${promptText} Is it ${defaultPromptValue}`, defaultPromptValue)
+console.log("What is thoust nameth?", answer)
+
+// readline node.js example
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('What is your name? ', (answer) => {
+    console.log(`Hello ${answer}`);
+    rl.close();
+});
