49ce682d69648d3006d775b52900d14d0e3299ab
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-16 19:14:23 -0500

    promises

--- script.js
+++ script.js
@@ -28,15 +28,71 @@
 
 
 // CALLBACK EXAMPLES
-function greeting(name, age, favColor) {
-    console.log(`Hello ${name}! You are ${age} years old and your favorite color is ${favColor}.`);
+// function greeting(name, age, favColor) {
+//     console.log(`Hello ${name}! You are ${age} years old and your favorite color is ${favColor}.`);
+// }
+
+// function processUserInput(z) {
+//     let y = prompt("What is your name?");
+//     let a = prompt("What is your age?");
+//     let b = prompt("What is your favorite color?");
+//     z(y, a, b);
+// }
+
+// processUserInput(greeting);
+
+// PROMISES EXAMPLE
+const myPromise = new Promise((res, rej) => {
+    let success = true; // to simulate the succecss or failure of an async operation
+    if (success) {
+        res("OPERATION WAS A SUCCESS");
+    } else {
+        rej("OPERATION FAILED");
+    }
+})
+
+// PROMISES .then() and .catch() EXAMPLE
+
+myPromise
+    .then((message) => {
+        console.log("working from the .then()")
+        console.log(message)
+    })
+    .catch((error) => {
+        console.log("NOT working from the .catch()")
+        console.log(error)
+    })
+// .finally(console.log("this is the end of the .then() and .catch() example")); // optional
+
+function stepOne() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Step One is complete");
+            resolve();
+        }, 1000)
+    })
 }
 
-function processUserInput(z) {
-    let y = prompt("What is your name?");
-    let a = prompt("What is your age?");
-    let b = prompt("What is your favorite color?");
-    z(y, a, b);
+function stepTwo() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Step Two is complete");
+            resolve();
+        }, 1000)
+    })
 }
 
-processUserInput(greeting);
+function stepThree() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Step Three is complete");
+            resolve();
+        }, 1000)
+    })
+}
+
+stepOne()
+    .then(console.log("All steps are complete"))
+    .then(stepThree)
+    .then(stepTwo)
+    .catch(error => console.log(error))
